a07a7a6ad3367999eabe03a88af831468718addc
LordMalto <101904063+LordMalto@users.noreply.github.com>
2022-06-23 09:56:30 +0200

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a Number: \n"))
+
+if num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
+elif num % 5 == 0:
+    print("Buzz")
+elif num % 3 == 0:
+    print("Fizz")
+else:
+    print("Number is not divideable by 3 or 5!")
