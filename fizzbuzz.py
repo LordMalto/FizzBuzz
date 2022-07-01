3f9fa1c5b0ab359c07d6c11f5068f0b7240cbc03
LordMalto <101904063+LordMalto@users.noreply.github.com>
2022-07-01 10:56:27 +0200

    Added loop

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,11 @@
-num = int(input("Enter a Number: \n"))
+endnum = int(input("Enter a Number: \n"))
 
-if num % 3 == 0 and num % 5 == 0:
-    print("FizzBuzz")
-elif num % 5 == 0:
-    print("Buzz")
-elif num % 3 == 0:
-    print("Fizz")
-else:
-    print("Number is not divideable by 3 or 5!")
+for num in range (endnum):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    else:
+    print(num)
